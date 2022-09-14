14e38cdf4414daefd9b7e5327a9c1b6b038a5c61
Kerim <90871131+k4rimDev@users.noreply.github.com>
2022-09-14 15:31:07 +0400

    Update settings.py

--- backend/online_project_share/settings.py
+++ backend/online_project_share/settings.py
@@ -219,10 +219,9 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
-DEFAULT_FROM_EMAIL = os.getenv('EMAIL_HOST_USER')
-
+EMAIL_HOST_USER = '321kerim132@gmail.com'
+EMAIL_HOST_PASSWORD = 'upopswhvjzqxspxv'
+DEFAULT_FROM_EMAIL = '321kerim132@gmail.com'
 
 
 
